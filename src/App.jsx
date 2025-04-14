0cb3fbd452a8ab9ae793ade4eccca3d7e7bb97fc
Praveen <praveennandipati@gmail.com>
2025-04-14 12:17:27 +0530

    commit basse files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+function App() {
+ 
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
